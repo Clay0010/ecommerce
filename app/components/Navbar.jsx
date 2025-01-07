ae4abde2bc823678eda5fe3679be5709a08155e7
Clay0010 <ahmedayad328@gmail.com>
2025-01-08 01:47:26 +0300

    feat: add search modal

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -8,11 +8,16 @@ import { IoIosAddCircleOutline } from "react-icons/io";
 import { useSelector } from "react-redux";
 import Cookies from "js-cookie";
 import { useState, useEffect } from "react";
+import { SearchModal } from "./SearchModal";
+import ApiClient from "../utils/apiClient";
+import { debounce } from "../utils/debounce";
 
 export const Navbar = () => {
   const [accessToken, setAccessToken] = useState(null);
   const [isHydrated, setIsHydrated] = useState(false);
-
+  const [showSearchModal, setShowSearchModal] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
   const totalQuantity = useSelector((state) => state.cart.totalQuantity);
 
   useEffect(() => {
@@ -21,14 +26,44 @@ export const Navbar = () => {
     setAccessToken(token);
     // Ensure the client is hydrated
     setIsHydrated(true);
+
+    // const fetchProcuts = async () => {
+    //   const response = await ApiClient.get("./products");
+    //   console.log(response.data.result.products);
+    //   setProducts(response.data.result.products);
+    // };
+    // fetchProcuts();
   }, []);
 
   if (!isHydrated) {
     // Avoid rendering mismatched HTML during SSR
     return null;
   }
+
+  const handleSearch = debounce(async (query) => {
+    if (!query.trim()) {
+      setSearchResult([]);
+      return;
+    }
+    const response = await ApiClient.get("./products");
+    const allProducts = response.data.result.products;
+
+    const filteredResult = allProducts.filter((product) =>
+      product.name.toLowerCase().includes(query.toLowerCase())
+    );
+
+    setSearchResult(filteredResult);
+    // console.log(filteredResult);
+  }, 500);
+
+  const handleChange = (e) => {
+    const query = e.target.value;
+    setSearchQuery(query);
+    handleSearch(query);
+  };
+
   return (
-    <div className="border p-4 flex items-center px-20">
+    <div className="border p-4 flex items-center px-20 bg-white z-50 sticky top-0">
       {/* Left Section */}
       <div className="flex gap-7 flex-grow">
         <Link href="/shop">Shop</Link>
@@ -38,17 +73,38 @@ export const Navbar = () => {
       </div>
 
       {/* Center Section */}
-      <div className="flex-shrink-0 text-2xl font-bold">
+      <div className="flex-shrink-0 text-2xl font-bold mr-[-40px]">
         <Link href="/">TechWorld</Link>
       </div>
 
       {/* Right Section */}
       <div className="flex gap-4 flex-grow items-center justify-end">
-        <input
-          type="text"
-          placeholder="Search"
-          className="border p-2 w-64 rounded-md"
-        />
+        <div
+          className="relative"
+          onBlur={(e) => {
+            if (!e.currentTarget.contains(e.relatedTarget)) {
+              setShowSearchModal(false);
+            }
+          }}
+        >
+          <input
+            type="text"
+            placeholder="Search"
+            className={`border p-2 w-64 rounded-md`}
+            value={searchQuery}
+            onFocus={() => setShowSearchModal(true)}
+            onChange={handleChange}
+          />
+          {showSearchModal && searchQuery.trim() && (
+            <SearchModal
+              onClickOutside={() => setShowSearchModal(false)}
+              searchResult={searchResult}
+              setShowSearchModal={setShowSearchModal}
+              setSearchQuery={setSearchQuery}
+            />
+          )}
+        </div>
+
         <Link href="/cart" className="relative">
           <CiShoppingCart
             size={24}
---
+++ app/components/SearchModal.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { useRouter } from "next/navigation";
+
+export const SearchModal = ({
+  onClickOutside,
+  searchResult,
+  setShowSearchModal,
+  setSearchQuery,
+}) => {
+  const router = useRouter();
+
+  const openProductPage = (id) => {
+    router.push(`/product/${id}`);
+    setShowSearchModal(false);
+    setSearchQuery("");
+  };
+  return (
+    <div
+      className="border p-3 w-96 bg-white absolute top-full right-0 z-20 rounded border-black"
+      tabIndex={-1} // Make the modal focusable
+      onBlur={(e) => {
+        if (!e.currentTarget.contains(e.relatedTarget)) {
+          onClickOutside();
+        }
+      }}
+    >
+      <p className="uppercase text-xs pb-1 border-b-[1px] border-black">
+        search result
+      </p>
+
+      {searchResult.length > 0 ? (
+        searchResult.map((product) => {
+          return (
+            <div
+              className="flex gap-4 mt-2 hover:cursor-pointer border-b-[1px] pb-2 items-center"
+              key={product.id}
+              onClick={() => openProductPage(product.id)}
+            >
+              <img
+                src="/headphone.jpg"
+                alt="product image"
+                className="w-16 h-16 object-cover bg-center"
+              />
+              <span>
+                <p className="text-sm pb-1 font-light">{product.name}</p>
+                <p className="text-sm font-semibold">${product.price}</p>
+              </span>
+            </div>
+          );
+        })
+      ) : (
+        <h1 className="text-sm mt-2">No Products Found</h1>
+      )}
+    </div>
+  );
+};
